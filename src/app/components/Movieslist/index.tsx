1451821b611c4b3df2093a04f0a1226a54040856
Luis Alberto <euokira2005@gmail.com>
2024-08-01 01:12:20 -0300

    Main

---
+++ src/app/components/Movieslist/index.tsx
@@ -0,0 +1,9 @@
+import "./index.scss";
+
+export default function Movieslist(){
+    return(
+        <ul className="movies-list">
+            <li></li>
+        </ul>
+    );
+}
---
+++ src/app/components/Navbar/index.tsx
@@ -0,0 +1,9 @@
+import "./index.scss"
+
+export default function Navbar(){
+    return(
+        <nav className="navbar">
+            <h1 className="page-title">Filmes</h1>
+        </nav>
+    )
+}
